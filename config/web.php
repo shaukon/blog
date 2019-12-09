33032583a27aab8380f0289aac2d206b28ec9cec
shaukon <shaukon@163.com>
2019-12-09 16:11:42 +0800

    add home

--- config/web.php
+++ config/web.php
@@ -1,5 +1,9 @@
 <?php  return array (
-  'wet_site' => '谢小康-博客',
-  'web_count' => '欢迎你来到我的博客',
+  'web_title' => '谢小康个人博客',
+  'web_count' => '百度统计',
   'web_status' => '1',
+  'seo_title' => '优秀个人技术博客',
+  'keywords' => 'php教程,web前端开发,mysql教程,linux学习,个人博客,技术博客,谢小康',
+  'description' => '谢小康（Shaukon）的博客是一个优秀的个人技术博客。谢小康的个人博客记录了Linux学习和Linux服务器入门教程，PHP开发和PHP主流框架实战技巧，Web前端开发和Web框架学习，MySQL基础和MySQL运维实用技巧，MongoDB、Redis、Memcache等NoSQL数据库教程以及个人的人生经历和观点。',
+  'copyright' => 'Design by 谢小康 <a href="http://www.xiexiaokang.com/" target="_blank">http://www.xiexiaokang.com</a>',
 );
